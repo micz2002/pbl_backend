7bbe59b4bd56b7669e734c8806f4f6f8e30091b9
Jakub Fałek <jakub2002pl@wp.pl>
2024-06-27 00:33:53 +0200

    Initial commit

---
+++ src/main/java/pl/pbl/smiecioapka_backend/SmiecioapkaBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.pbl.smiecioapka_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmiecioapkaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmiecioapkaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pbl/smiecioapka_backend/SmiecioapkaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pbl.smiecioapka_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmiecioapkaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
